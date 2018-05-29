1ecedf7afdea45e3b98cff67ebbc15894bb41152
teripanda <teri.london@gmail.com>
2018-05-29 11:34:38 -0700

    set up server.js ty justiin

--- server.js
+++ server.js
@@ -14,3 +14,8 @@ app.use(express.static('public'));
 app.get('/', function homepage (req, res) {
   res.sendFile('views/index.html', { root : __dirname });
 });
+
+// tell the app to listen on a port so that the server will start
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is running on http://localhost:3000/');
+});
