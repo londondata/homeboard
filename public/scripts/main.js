6e53aadffe8e732386ff1d23d14968e2d30adf73
teripanda <teri.london@gmail.com>
2018-05-30 14:29:34 -0700

    modals are working exclamation point

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,6 +7,19 @@ var day = ("0" + now.getDate()).slice(-2);
 var month = ("0" + (now.getMonth() + 1)).slice(-2);
 var today = now.getFullYear()+"-"+(month)+"-"+(day) ;
 
+
+
+$('#householdModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('whatever') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.modal-title').text('add household user')
+  // modal.find('.modal-body input').val(recipient)
+});
+
+
 $('#date').val(today);
     $("#addHome").on("click", (e)=>{
         e.preventDefault();
