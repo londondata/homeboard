cb293b261f2793861d3e3d264f1eabdb2d7668a6
teripanda <teri.london@gmail.com>
2018-05-31 12:42:22 -0700

    css stylings, adding jquery

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,27 +7,22 @@ var day = ("0" + now.getDate()).slice(-2);
 var month = ("0" + (now.getMonth() + 1)).slice(-2);
 var today = now.getFullYear()+"-"+(month)+"-"+(day) ;
 
-
-
+//modals:
 $('#householdModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget) // Button that triggered the modal
-  var recipient = button.data('whatever') // Extract info from data-* attributes
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var button = $(event.relatedTarget)
+  var recipient = button.data('whatever')
   var modal = $(this)
   modal.find('.modal-title').text('add household user')
 });
 
 $('#choresModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget) // Button that triggered the modal
-  var recipient = button.data('whatever') // Extract info from data-* attributes
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var button = $(event.relatedTarget)
+  var recipient = button.data('whatever')
   var modal = $(this)
   modal.find('.modal-title').text('add chores')
 });
 
-
+//date:
 $('#date').val(today);
     $("#addHome").on("click", (e)=>{
         e.preventDefault();
@@ -35,6 +30,26 @@ $('#date').val(today);
         console.log("sanity check")
         $('#addHomeForm').css("display","inline-block");
     });
+
+//add messages to wall:
+
+$('#addMessage').on('click', function(e) {
+    e.preventDefault();
+    console.log('be i working?')
+
+    var newMessage = $('#addMessage');
+
+    // var defaultPlaceholder = $(newTaskField).attr('placeholder');
+
+    // var newTask = $(newTaskField).val();
+
+   $('#messages').append(newMessage);
+
+   // $(addMessage).val('');
+   
+   // $(addMessage).attr('placeholder', defaultPlaceholder);
+});
+
 
 
 
